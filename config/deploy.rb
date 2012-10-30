48acbe85f95518e8a8b5c7f4827f09d94c9d223b
viy <pervak.s@gmail.com>
2012-10-30 23:11:57 +0200

    vps release 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:viy/#{application}.git"
+set :repository, "git@github.com:viy/dream_result.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
