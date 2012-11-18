8c699b2be6bdab96c8a869ed8bec8d811ad829b2
viy <pervak.s@gmail.com>
2012-11-18 16:32:04 +0200

    strict tagging

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,6 @@ class ProductsController < ApplicationController
   def index
     I18n.locale = 'ru'
     @products = Product.page(params[:page]).per(9)
-    @products = @products.tagged_with( params[:tag], :match_all => true ) if params[:tag]
+    @products = @products.tagged_with( params[:tag]) if params[:tag]
   end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,7 +2,7 @@ class ServicesController < ApplicationController
   def index
     I18n.locale = 'ru'
     @services = Service.filtered_by_type(params[:type]).page(params[:page]).per(8)
-    @services = @services.tagged_with( params[:tag], :match_all => true ) if params[:tag]
+    @services = @services.tagged_with( params[:tag] ) if params[:tag]
     respond_to do |format|
       format.html
       format.js {render :partial => "index"}
