b0bfb2bf5172c4569dffe2478536ef88e1313e08
viy <pervak.s@gmail.com>
2012-12-03 01:27:28 +0200

    pre release 2

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,27 @@
 module ServicesHelper
+  def get_price_name(service)
+    if service
+      if service == 0
+        t('forbid')
+      else
+        "#{service} #{t 'curency'}"
+      end
+    else
+      t('included')
+    end
+  end
+
+  def get_price_class(service)
+    if service.nil? || service == 0
+      ""
+    else
+      "buy"
+    end
+  end
+
+  def get_active_tab(tab)
+    if params[:program] == tab.to_s || (params[:program].nil? && tab == :all)
+      "active"
+    end
+  end
 end
